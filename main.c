05d1a6ceb7774c0070e44d95e2e17925d56a1ad6
ester <95254487+esterrpereira@users.noreply.github.com>
2022-04-02 12:58:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main(void) {
   int codigo,quantidade;
    float preco;
-  printf("Cardápio\n \n Cachorro Quente R$ 10.20 (cod 100)\n Bauru R$ 10.30 (cod 101)\n Batata Frita R$ 10.50 (cod 102)\n Hambúrguer  R$ 10.20 (cod 103)\n Cheeseburger R$ 10.30 (cod 104)\n Refrigerante R$ 3.00 (cod 105)\n\n" );
+  printf("\t\tCardápio\n \n Cachorro Quente R$ 10.20 (cod 100)\n Bauru R$ 10.30 (cod 101)\n Batata Frita R$ 10.50 (cod 102)\n Hambúrguer  R$ 10.20 (cod 103)\n Cheeseburger R$ 10.30 (cod 104)\n Refrigerante R$ 3.00 (cod 105)\n\n" );
   printf("Digite o código: ");
   scanf("%d", &codigo);
   printf("Digite a quantidade: ");
